02a9ee4748bbdd731feddf1d43fc92b6452c38fa
Vitaly Bochkarev <vitalybochkarev@Book.local>
2022-02-26 00:49:29 +0700

    fisrt commit

---
+++ app.js
@@ -0,0 +1,64 @@
+//1 Variables
+//camelCase
+// const firstName = 'Vit' //string
+// const  age = 40 //number
+// const isProgrammer = true //boolean
+
+// const _ = 'private'
+// const $ = 'some value'
+
+//const if = 'no' //err
+// const withNum = '5'
+// const 5withNum = '5' //err
+
+//2 Mutation
+// console.log('Person\'s name: ' + firstName + ' and the person\'s age: ' + age)
+// alert('Person\'s name: ' + firstName + ' and the person\'s age: ' + age)
+
+// const lastName = prompt('Enter last name')
+// alert(firstName + ' ' + lastName)
+
+//3 Operators
+// let currentYear = 2022
+// const birthYear = 1981
+
+// const age = currentYear - birthYear
+// console.log(age)
+
+// const a = 10
+// const b = 5
+
+// let c = 32
+// c = c + a
+// c += a
+// c -= a
+// c *= a
+// c /= a
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(currentYear++)
+// console.log(--currentYear)
+
+//4 Data types
+// const firstName = 'Vit'
+// const  age = 40
+// const isProgrammer = true
+// let x
+// console.log(typeof firstName) //string
+// console.log(typeof age) //number
+// console.log(typeof isProgrammer) //boolean
+// console.log(typeof x) //undefined
+// console.log(typeof null) //object
+
+//5 Operators precedence
+// const fullAge = 40
+// const birthYear = 1981
+// const currentYear = 2022
+
+// > < >= <=
+// const isFullAge = currentYear - birthYear >= fullAge //41 >= 40 => true
+// console.log(isFullAge) //true
+
