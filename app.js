7fa53f7e827a87a43a37819e1c5f7ba2bd67820e
VitBoch <vitaliybochkarev@mail.ru>
2022-03-18 16:15:51 +0700

    9

--- app.js
+++ app.js
@@ -62,3 +62,73 @@
 // const isFullAge = currentYear - birthYear >= fullAge //41 >= 40 => true
 // console.log(isFullAge) //true
 
+<<<<<<< HEAD
+=======
+//6 Conditional statements
+// const  courseStatus = 'pending' //ready, fail, pending
+//
+// if (courseStatus === 'ready') {
+//     console.log('The course is ready, and you can take it')
+// } else if (courseStatus === 'pending') {
+//     console.log('The course is currently under development')
+// } else {
+//     console.log('Course failed')
+// }
+//
+// const isReady = true
+
+// if (isReady) {
+//     console.log('Everything is ready')
+// } else {
+//     console.log('Everything is not ready')
+// }
+
+// // Ternary expression
+// isReady ? console.log('Everything is ready') : console.log('Everything is not ready')
+
+// const num1 = 42 //number
+// const num2 = '42' //string
+//
+// console.log(num1 == num2) //true
+// console.log(num1 === num2) //false
+
+//7 Boolean logic
+// console.log(true && true) //true
+// console.log(false && true) //false
+// console.log(true && false) //false
+// console.log(false && false) //false
+//
+// console.log(true || true) //true
+// console.log(false || true) //true
+// console.log(true || false) //true
+// console.log(false || false) //false
+
+// console.log(!true) //false
+// console.log(!!true) //true
+
+//8 function
+// function calculateAge(year) {
+//     return 2022 - year
+}
+
+// const myAge = calculateAge(1981)
+// console.log(myAge)
+
+// function logInfoAbout(name, year) {
+//     const age = calculateAge(year)
+//
+//     if (age > 0) {
+//         console.log('A person named ' + name + ' now has an age ' + age)
+//     } else {
+//         console.log('Actually this is the future')
+//     }
+// }
+//
+// logInfoAbout('VitBoch', 1981)
+// logInfoAbout('Ivan', 1990)
+// logInfoAbout('Spyder man', 2395)
+
+//9 Array
+
+
+>>>>>>> 3d09660 (8)
